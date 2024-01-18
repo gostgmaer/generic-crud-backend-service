903707df627c156b0460722354356345b81ab651
Kishor Sarkar <kishor.sarkar@inadev.com>
2024-01-18 20:17:21 +0530

    changed

--- src/config/setting.js
+++ src/config/setting.js
@@ -4,26 +4,6 @@ const refressSecret = process.env.JWT_REFRESH_SECRET;
 const serverPort = process.env.PORT;
 const collection = process.env.COLLECTION;
 
-// # Nodemailer configuration
-
-const mailService = process.env.EMAIL_SERVICE;
-const mailUserName = process.env.EMAIL_USERNAME;
-const mailPassword = process.env.EMAIL_PASSWORD;
-const emailName = process.env.EMAIL_NAME;
-
-// # Mailchimp API key and list ID
-const mailchimpKey = process.env.MAILCHIMP_API_KEY;
-const mailchimpList = process.env.MAILCHIMP_LIST_ID;
-
-// # Client Application Name
-const applicaionName = process.env.APPLICATION_NAME;
-
-// #client urls
-
-const host = process.env.LOGINHOST;
-const loginPath = process.env.CLIENTLOGINPAGE;
-const resetPath = process.env.CLIENTRESETPASSURL;
-const confirmPath = process.env.CLIENTCONFIRMURL;
 const authHost = process.env.AUTHHOST;
 module.exports = {
   dbUrl,

6343f1c930265ce1bf5252c7c4c7cc2274bafab4
Kishor Sarkar <kishor.sarkar@inadev.com>
2024-01-18 20:51:24 +0530

    updated

--- src/controller/entity/controller.js
+++ src/controller/entity/controller.js
@@ -72,10 +72,10 @@ const get = async (req, res) => {
         { ...filterData.query, appId, containerId },
         { projection: filterData.arrayOfValues }
       )
-      .sort(filterData.options.sort)
-      .skip(filterData.options.skip)
-      .limit(parseInt(filterData.options.limit))
-      .toArray();
+      // .sort(filterData.options.sort)
+      // .skip(filterData.options.skip)
+      // .limit(parseInt(filterData.options.limit))
+      // .toArray();
     const totalCount = await genericSchema.countDocuments({
       ...filterData.query,
       appId,
